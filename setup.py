251b30a71ff0209fb7fc7717a19ccf3fb5b830f5
qxid <qxidddy@gmail.com>
2018-05-14 19:37:20 +0500

    Correct setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(
-   name='github_user_starred.py',
+   name='github_user_starred',
    version='1.0',
    description='github_user_starred - python script. Lists starred repositories by user',
    author='Andrew',
